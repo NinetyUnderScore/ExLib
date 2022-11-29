ab299e6e03f697eb04be846aa1c5a3b623f3eed8
_90_ <81204025+NinetyUnderScore@users.noreply.github.com>
2022-11-28 19:54:05 -0500

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -23,8 +23,20 @@ function LaunchUA(Vec3, Force, Loops)
         for i,v in pairs(workspace:GetDescendants()) do
             if v:IsA("BasePart") then
                 if isnetworkowner(v) and v.Parent ~=  Char and v.Parent.Parent ~= Char then
-                    v.Orientation = Vector3.new(0,0,0)
                     v.Velocity = ((Vec3 - v.Position).Unit) * Vector3.new(Force,Force,Force)
+                end
+            end
+        end
+        wait()
+    end
+end
+
+function VelocityUA(Vec3, Loops)
+    for count = 0, Loops do
+        for i,v in pairs(workspace:GetDescendants()) do
+            if v:IsA("BasePart") then
+                if isnetworkowner(v) and v.Parent ~=  Char and v.Parent.Parent ~= Char then
+                    v.Velocity = Vec3
                 end
             end
         end
